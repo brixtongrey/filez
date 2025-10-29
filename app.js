13d5266630dff8f7aaf127f27e3019025f1982a7
Brixtongrey <brixtonaruda@gmail.com>
2025-10-28 20:38:42 -0600

    feat: app.js file

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 import express from "express";
 const app = express();
+
+app.use(express.json());
+
+app.use("/folders", foldersRouter);
+app.use("/files", filesRouter);
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+});
+
 export default app;

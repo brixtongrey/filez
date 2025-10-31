45f5df56fb0b795ec34f046dfc7b683960fcb137
Brixtongrey <brixtonaruda@gmail.com>
2025-10-30 19:02:57 -0600

    feat: added get /folders

--- routes/folders.js
+++ routes/folders.js
@@ -3,4 +3,14 @@ import db from "../db/client.js";
 
 const router = express.Router();
 
+// GET /folders - all folders
+router.get("/", async (req, res, next) => {
+  try {
+    const { rows } = await db.query("SELECT * FROM folders");
+    res.json(rows);
+  } catch (err) {
+    next(err);
+  }
+});
+
 export default router;
